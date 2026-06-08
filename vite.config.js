6898a6544db41d4ccda524d551421ced92e1b7b8
RiceFish <shiiixx8@gmail.com>
2026-06-09 00:24:35 +0800

    精簡 vite.config.js，移除不必要的導入與插件，確保 base 路徑正確

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [vue()],
+  // 👇 加上這一行，請確保前後都有斜線，並且大小寫與 GitHub 儲存庫名稱完全一致
+  base: '/TAOBAO-Pfishing-vue/' 
 })
